c105529cb99081f04a7d05bfd30fa3c32c87e84a
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-07-30 21:20:26 -0700

    Update astrophotography.sh

--- astrophotography.sh
+++ astrophotography.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-pic_timer=300
+pic_timer=60
 fileformat=.jpg
+fileformat_arg=--jpeg 85
 
 #while [true]
 #do
     # Timestamp
     stamp=$(date +"%Y-%m-%d-%H-%M-%S")
-    name=$stamp$fileformat
 
     # Take a pic
-    #fswebcam -r 640x480 --jpeg 85 -D 1 ./photos/$name
-    fswebcam -d /dev/video2 -r 3264x2448 --jpeg 85 -D 1 ./photos/$name --no-banner
+    #fswebcam -r 640x480 --jpeg 85 -D 1 ./photos/$stamp$fileformat
+    fswebcam -d /dev/video2 -r 3264x2448 $fileformat_arg -D 1 ./photos/$stamp$fileformat --no-banner
 
     # Sleep
     sleep $pic_timer
