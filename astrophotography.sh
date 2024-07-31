c2cf4c92f1992cb0364ba7f57ac28d33388ba086
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-07-30 23:27:36 -0700

    png and gcloud

--- astrophotography.sh
+++ astrophotography.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 pic_timer=60
-fileformat=.jpg
+#fileformat=.jpg
+fileformat=.png
 device=$(v4l2-ctl --list-devices | grep -i 'FIBONAX Nova800' -A 1 | grep -i '/dev/video')
+foldername=$(date +"%Y-%m-%d-%H-%M-%S")
+mkdir -p ./photos/$foldername
 
 while true; do
     # Timestamp
     stamp=$(date +"%Y-%m-%d-%H-%M-%S")
 
     # Take a pic
-    fswebcam -d $device -r 3264x2448 --jpeg 85 -D 1 ./photos/$stamp$fileformat --no-banner
+    #fswebcam -d $device -r 3264x2448 --jpeg 95 -D 1 $folderpath$stamp$fileformat --no-banner
+    fswebcam -d $device -r 3264x2448 --png 9 ./photos/$foldername/$stamp$fileformat --no-banner
+
+    # Upload to Cloud Storage
+    #gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://DESTINATION_BUCKET_NAME/$foldername/$stamp$fileformat
 
     # Sleep
     sleep $pic_timer
