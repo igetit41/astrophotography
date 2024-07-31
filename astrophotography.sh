71b828ce51dea4cef64cd470ed005b9a0f0169ee
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-07-30 22:15:39 -0700

    Update astrophotography.sh

--- astrophotography.sh
+++ astrophotography.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
 pic_timer=60
 fileformat=.jpg
+device=$(v4l2-ctl --list-devices | grep -i 'FIBONAX Nova800' -A 1)
 
 while true; do
     # Timestamp
     stamp=$(date +"%Y-%m-%d-%H-%M-%S")
 
     # Take a pic
-    #fswebcam -r 640x480 --jpeg 85 -D 1 ./photos/$stamp$fileformat
-    fswebcam -d /dev/video2 -r 3264x2448 --jpeg 85 -D 1 ./photos/$stamp$fileformat --no-banner
+    fswebcam -d $device -r 3264x2448 --jpeg 85 -D 1 ./photos/$stamp$fileformat --no-banner
 
     # Sleep
     sleep $pic_timer
