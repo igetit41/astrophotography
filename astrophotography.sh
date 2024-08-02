413b9698875b3cd3847c641dc28b360086ae443a
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-08-02 16:30:39 -0700

    all gcloud commands together

--- astrophotography.sh
+++ astrophotography.sh
@@ -7,15 +7,14 @@ device=$(v4l2-ctl --list-devices | grep -i 'FIBONAX Nova800' -A 1 | grep -i '/de
 foldername=$(date +"%Y-%m-%d-%H-%M-%S")
 mkdir -p ./photos/$foldername
 
-path_to_google_cloud_sdk=/home/d3
-export PATH="$path_to_google_cloud_sdk/google-cloud-sdk/bin:$PATH"
+#path_to_google_cloud_sdk=/home/d3
+#export PATH="$path_to_google_cloud_sdk/google-cloud-sdk/bin:$PATH"
 
 path_to_gcloud_auth=../gcloud_auth
 sudo chmod +x $path_to_gcloud_auth/gcloud_auth.sh
-glcoud_auth=$(/bin/bash $path_to_gcloud_auth/gcloud_auth.sh)
 echo "glcoud_auth: $glcoud_auth"
 
-service_account=photos-push@sandcastle-401716.iam.gserviceaccount.com
+#service_account=photos-push@sandcastle-401716.iam.gserviceaccount.com
 #activate_account=(gcloud auth activate-service-account --key-file=./$service_account_keyfile_name --project=$service_account_project)
 #echo "$activate_account"
 
@@ -28,8 +27,10 @@ while true; do
     fswebcam -d $device -r 3264x2448 --png 9 ./photos/$foldername/$stamp$fileformat --no-banner
 
     # Upload to Cloud Storage
-    echo "gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat"
-    upload=$(gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat)
+    echo "gcloud_upload: gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat"
+    #upload=$(gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat)
+    gcloud_upload="gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat"
+    upload=$(/bin/bash $path_to_gcloud_auth/gcloud_auth.sh -gcloud_command "$gcloud_upload")
     echo "$upload"
 
     # Sleep
--- gcloud_auth/gcloud_auth.sh
+++ gcloud_auth/gcloud_auth.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+while getopts gcloud_command: flag
+do
+    case "${flag}" in
+        gcloud_command) gcloud_command=${OPTARG};;
+    esac
+done
+echo "gcloud_command: $gcloud_command"
+
 #Copy this whole folder one directory up, add the json keyfile, and replace the following variables.
 service_account=<service_account@project.iam.gserviceaccount.com>
 service_account_keyfile_name=<project-rando.json>
@@ -12,3 +21,6 @@ echo "$set_account"
 
 activate_account=$(gcloud auth activate-service-account --key-file=./$service_account_keyfile_name --project=$service_account_project)
 echo "$activate_account"
+
+gcloud_command_result=$($gcloud_command)
+echo "$gcloud_command_result"
