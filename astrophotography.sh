8495a60c31033dd33c03ec29136af5284f9d657f
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-08-02 17:24:17 -0700

    Update astrophotography.sh

--- astrophotography.sh
+++ astrophotography.sh
@@ -7,16 +7,9 @@ device=$(v4l2-ctl --list-devices | grep -i 'FIBONAX Nova800' -A 1 | grep -i '/de
 foldername=$(date +"%Y-%m-%d-%H-%M-%S")
 mkdir -p ./photos/$foldername
 
-#path_to_google_cloud_sdk=/home/d3
-#export PATH="$path_to_google_cloud_sdk/google-cloud-sdk/bin:$PATH"
-
 path_to_gcloud_auth=../gcloud_auth
 sudo chmod +x $path_to_gcloud_auth/gcloud_auth.sh
 echo "glcoud_auth: $glcoud_auth"
-
-#service_account=photos-push@sandcastle-401716.iam.gserviceaccount.com
-#activate_account=(gcloud auth activate-service-account --key-file=./$service_account_keyfile_name --project=$service_account_project)
-#echo "$activate_account"
 
 while true; do
     # Timestamp
@@ -27,7 +20,7 @@ while true; do
     fswebcam -d $device -r 3264x2448 --png 9 ./photos/$foldername/$stamp$fileformat --no-banner
 
     # Upload to Cloud Storage
-    gcloud_upload="gcloud storage cp ./photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat"
+    gcloud_upload="gcloud storage cp ../astrophotography/photos/$foldername/$stamp$fileformat gs://sandcastle-401716-photos/$foldername/$stamp$fileformat"
     echo "gcloud_upload: $gcloud_upload"
     
     upload=$(/bin/bash $path_to_gcloud_auth/gcloud_auth.sh "$gcloud_upload")
