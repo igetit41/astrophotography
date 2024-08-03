0fece4ca937516868bb96062d43f76aa26e1462d
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-08-02 17:51:42 -0700

    Update gcloud_auth.sh

--- gcloud_auth/gcloud_auth.sh
+++ gcloud_auth/gcloud_auth.sh
@@ -8,14 +8,14 @@ service_account=<service_account@project.iam.gserviceaccount.com>
 service_account_keyfile_name=<project-rando.json>
 service_account_project=<project>
 path_to_google_cloud_sdk=<objective path to google-cloud-sdk>
-path_to_git=<objective path to git>
+gcloud_auth=<objective path to gcloud_auth>
 
 export PATH="$path_to_google_cloud_sdk/google-cloud-sdk/bin:$PATH"
 
 set_account=$(gcloud config set account $service_account)
 echo "set_account: $set_account"
 
-activate_account=$(gcloud auth activate-service-account --key-file=$path_to_git/$service_account_keyfile_name --project=$service_account_project)
+activate_account=$(gcloud auth activate-service-account --key-file=$path_to_git/gcloud_auth/$service_account_keyfile_name --project=$service_account_project)
 echo "activate_account: $activate_account"
 
 gcloud_command_result=$($gcloud_command)
