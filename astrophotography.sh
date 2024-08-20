47ccea15aad2b1a96b665e6c63727dd9225d1539
igetit41 <45726571+igetit41@users.noreply.github.com>
2024-08-19 20:36:11 -0700

    Update astrophotography.sh

--- astrophotography.sh
+++ astrophotography.sh
@@ -5,7 +5,7 @@ fileformat=.png
 path_to_gcloud_auth=../gcloud_auth
 gsbucket=sandcastle-401716-photos
 
-device_command="v4l2-ctl --list-devices | grep -i \'$1\' -A 1 | grep -i \'/dev/video\'"
+device_command="v4l2-ctl --list-devices | grep -i '${1}' -A 1 | grep -i '/dev/video'"
 device_resultx=$($device_command)
 
 device_result=$(v4l2-ctl --list-devices | grep -i 'USB 2.0 Camera' -A 1 | grep -i '/dev/video')
