0fac70c2a9c4f9ed6561f730c55e510a51fb07a6
igetit41 <45726571+igetit41@users.noreply.github.com>
2025-02-05 15:35:09 -0800

    more outputs

--- image_upload.sh
+++ image_upload.sh
@@ -17,34 +17,38 @@ while true; do
     if [ $? == 0 ]; then
 
         images_local=$(find $working_dir/photos -print)
+        echo "images_local: $images_local"
 
         gcloud_command="gcloud storage ls --recursive gs://$gsbucket/**"
         images_bucket=$(/bin/bash $working_dir/gcloud_auth/gcloud_auth.sh "$gcloud_command")
+        echo "images_bucket: $images_bucket"
 
         IFS='\n'
         read -ra images_local_array <<< "$images_local"
+        echo "images_local_array: $images_local_array"
         read -ra images_bucket_array <<< "$images_bucket"
+        echo "images_bucket_array: $images_bucket_array"
 
         local_prefix="$working_dir/photos/"
         local_prefix_length=${#local_prefix}
-        echo $local_prefix_length
+        echo "local_prefix_length: $local_prefix_length"
 
         bucket_prefix="gs://$gsbucket/"
         bucket_prefix_length=${#bucket_prefix}
-        echo $bucket_prefix_length
+        echo "bucket_prefix_length: $bucket_prefix_length"
 
         for image_local in $images_local_array;
         do
-            echo $image_local
+            echo "image_local: $image_local"
             image_local_trunk="${image_local:$local_prefix_length}"
-            echo $image_local_trunk
+            echo "image_local_trunk: $image_local_trunk"
             match_found="false"
 
             for image_bucket in $images_bucket_array;
             do
-                echo $image_bucket
+                echo "image_bucket: $image_bucket"
                 image_bucket_trunk="${image_bucket:$bucket_prefix_length}"
-                echo $image_bucket_trunk
+                echo "image_bucket_trunk: $image_bucket_trunk"
 
                 if [[ "$image_local_trunk" == "$image_bucket_trunk" ]]; then
                     match_found="true"
